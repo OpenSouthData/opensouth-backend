4790df5e5495f7acab4d83da622b6d157bf2324d
adeleke-code <femiadeleke601@gmail.com>
2024-08-07 22:14:20 +0100

    email bug fix

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -2,4 +2,4 @@
 
 bind = "0.0.0.0:4000"  # Specify the host and port
 workers = 4  # Number of worker processes
-timeout = 60 # Time in seconds before a worker is killed and restarted
+timeout = 60*10 # Time in seconds before a worker is killed and restarted
