8e32bc47d44a1eab32a4a666cec8210ff70ed117
adeleke-code <femiadeleke601@gmail.com>
2024-08-08 17:59:08 +0100

    prod --> staging

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,5 +1,9 @@
 # gunicorn_config.py
+import os
 
-bind = "0.0.0.0:4000"  # Specify the host and port
+port = os.getenv("PORT") 
+
+
+bind = f"0.0.0.0:{port}"  # Specify the host and port
 workers = 4  # Number of worker processes
 timeout = 60*10 # Time in seconds before a worker is killed and restarted
